5ea3af7932763ece451f3513df4ad73e2943ae82
Capybara <seanlyc@berkeley.edu>
2026-05-12 16:11:13 -0700

    update for python 3.11

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,6 @@
 from application import app
 from flask import render_template, request, json, jsonify
-from sklearn import preprocessing
-from sklearn.preprocessing import OneHotEncoder
 import requests
-import numpy
-import pandas as pd
 
 #decorator to access the app
 @app.route("/")
@@ -28,13 +24,10 @@ def carclassify():
     input_data = json.dumps({"buying": buying, "maint": maint, "doors": doors, "persons": persons, "lug_boot": lug_boot, "safety": safety})
 
     #url for car classification api
-    #url = "http://localhost:5000/api"
-    url = "https://dsm-car-model.herokuapp.com/api"
+    url = "http://localhost:5000/api"
 
- 
     #post data to url
     results =  requests.post(url, input_data)
 
     #send input values and prediction result to index.html for display
     return render_template("index.html", buying = buying, maint = maint, doors = doors, persons = persons, lug_boot = lug_boot, safety = safety, results=results.content.decode('UTF-8'))
-  
